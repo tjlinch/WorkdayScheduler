d55a5a5ea63badbf4435b01cc84bc665e12130a3
tjlinch <Tyler@Tylers-MacBook-Pro-8.local>
2022-08-15 18:32:14 -0600

    added timeblocks & current date

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+//Variable declarations
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+var mainEl = $('#main');
+console.log(mainEl.children);
+
+
+
+// var eightAmEl = $('#8amInput');
+// var nineAmEl = $('#9amInput');
+// var tenAmEl = $('#10amInput');
+// var elevenAmEl = $('#11amInput');
+// var twelvePmEl = $('#12pmInput');
+// var onePmEl = $('#1pmInput');
+// var twoPmEl = $('#2pmInput');
+// var threePmEl = $('#3pmInput');
+// var fourPmEl = $('#4pmInput');
+// var fivePmEl = 
+
+
+
+
+
